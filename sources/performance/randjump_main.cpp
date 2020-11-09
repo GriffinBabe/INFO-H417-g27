17155888620c80b2be79ced1d0bd788836959a26
Darius Couchard <darius.couchard@hotmail.com>
2020-11-09 15:44:16 +0100

    Added stream performance test executables and main files to CMakeLists.txt

---
+++ sources/performance/randjump_main.cpp
@@ -0,0 +1,7 @@
+#include <stream/input_stream.hpp>
+#include <stream/output_stream.hpp>
+
+int main(int argc, char** argv)
+{
+    // Parse arguments with Boost: https://www.boost.org/doc/libs/1_58_0/doc/html/program_options.html
+}
